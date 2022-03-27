77c0340ab05f0cde476c63f619227f49c4562733
Katrina Lenell Hill <khill365@gmail.com>
2022-03-27 15:05:02 -0700

    revised code to require cache object and return weather data

--- modules/weather.js
+++ modules/weather.js
@@ -4,8 +4,6 @@ const axios = require('axios');
 
 let cache = require('./cache.js');
 
-let cache = {};
-
 async function getWeather(request, response) {
   try {
     // create a key for what the user has searched for
@@ -19,10 +17,10 @@ async function getWeather(request, response) {
       console.log('Cache miss');
       // make a new API request
       const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
-      let apiForecasts = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+      let apiForecasts = await axios.get(url);
       let forecastArray = apiForecasts.data.data.map(weather => new Forecast(dataItem.valid_date, dataItem.weather.description));
-      cache[key] = {
-        date: forecastArray,
+      cache[key] = { 
+        data: forecastArray,
         timestamp: Date.now()
       }
       response.status(200).send(forecastArray);
@@ -36,8 +34,8 @@ async function getWeather(request, response) {
 
 function parseWeather(weatherData) {
   try {
-    const weatherSummaries = weatherData.data.map(day => {
-      return new Weather(day);
+    const weatherSummaries = weatherData.data.map(dataItem => {
+      return new Weather(dataItem.valid_date, dataItem.weather.description);
     });
     return Promise.resolve(weatherSummaries);
   } catch (e) {
@@ -45,17 +43,10 @@ function parseWeather(weatherData) {
   }
 }
 
-// return cache[key].data;
-
-cache[key] = {};
-cache[key].timestamp = Date.now();
-cache[key].data = axios.get(url)
-  .then(response => parseWeather(response.data));
-
 class Weather {
-  constructor(day) {
-    this.forecast = day.weather.description;
-    this.time = day.datetime;
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
   }
 }
 
